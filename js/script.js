528f57e52b8ed4efc080c44c373abab7838f0d6d
Anna Niewiarowska <niewiarowska.as@gmail.com>
2022-07-03 19:19:35 +0200

    image source changed, welcom init added, css sheet compited

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
- console.log("Witaj przyjacielu!");
+const welcome = () => {
+    console.log("Witaj przyjacielu!");
+    }
+    
+    welcome();
 
  let button = document.querySelector(".js-button");
- console.log("js-button")
  let image = document.querySelector(".js-image");
- console.log("js-image")
 
  button.addEventListener("click", () => {
      switch (button.innerText) {
          case "pokaż serwis chiński":
              button.innerText = "pokaż serwis polski";
-             image.src = "https://imgur.com/TTzzux8.jpeg";
+             image.src = "images/serwis_chinski.jpg";
              image.alt = "serwis herbaciany chiński";
              break;
          case "pokaż serwis polski":
              button.innerText = "pokaż serwis włoski";
-             image.src = "https://imgur.com/L7FEF3T.jpeg";
+             image.src = "images/serwis_polski.jpg";
              image.alt = "serwis kawowy polski";
              break;
          case "pokaż serwis włoski":
              button.innerText = "pokaż serwis francuski";
-             image.src = "https://imgur.com/cw8HGsQ.jpeg";
+             image.src = "images/serwis_wloski.jpg";
              image.alt = "serwis kawowy włoski";
              break;
          case "pokaż serwis francuski":
              button.innerText = "pokaż serwis chiński";
-             image.src = "https://imgur.com/why4zi4.jpeg";
+             image.src = "images/serwis_francuski.jpg";
              image.alt = "serwis herbaciany francuski";
              break;
      }

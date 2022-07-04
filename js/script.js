8dcea51c7e4cb5b5bb4c9f0726bc35a32558fa88
Anna Niewiarowska <niewiarowska.as@gmail.com>
2022-07-04 12:58:47 +0200

    js code writed as functions

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
-const welcome = () => {
+{
+    const welcome = () => {
     console.log("Witaj przyjacielu!");
     }
-    
-    welcome();
 
- let button = document.querySelector(".js-button");
- let image = document.querySelector(".js-image");
+    let onChangeClick = () => {
+        let image = document.querySelector(".js-image");
+        const button = document.querySelector(".js-button");
+          switch (button.innerText) {
+            case "pokaż serwis chiński":
+                button.innerText = "pokaż serwis polski";
+                image.src = "images/serwis_chinski.jpg";
+                image.alt = "serwis herbaciany chiński";
+                break;
+            case "pokaż serwis polski":
+                button.innerText = "pokaż serwis włoski";
+                image.src = "images/serwis_polski.jpg";
+                image.alt = "serwis kawowy polski";
+                break;
+            case "pokaż serwis włoski":
+                button.innerText = "pokaż serwis francuski";
+                image.src = "images/serwis_wloski.jpg";
+                image.alt = "serwis kawowy włoski";
+                break;
+            case "pokaż serwis francuski":
+                button.innerText = "pokaż serwis chiński";
+                image.src = "images/serwis_francuski.jpg";
+                image.alt = "serwis herbaciany francuski";
+                break;
+          }
+    } 
 
- button.addEventListener("click", () => {
-     switch (button.innerText) {
-         case "pokaż serwis chiński":
-             button.innerText = "pokaż serwis polski";
-             image.src = "images/serwis_chinski.jpg";
-             image.alt = "serwis herbaciany chiński";
-             break;
-         case "pokaż serwis polski":
-             button.innerText = "pokaż serwis włoski";
-             image.src = "images/serwis_polski.jpg";
-             image.alt = "serwis kawowy polski";
-             break;
-         case "pokaż serwis włoski":
-             button.innerText = "pokaż serwis francuski";
-             image.src = "images/serwis_wloski.jpg";
-             image.alt = "serwis kawowy włoski";
-             break;
-         case "pokaż serwis francuski":
-             button.innerText = "pokaż serwis chiński";
-             image.src = "images/serwis_francuski.jpg";
-             image.alt = "serwis herbaciany francuski";
-             break;
-     }
- });
- 
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", onChangeClick);
+        
+        welcome();
+    }
+
+    init();
+ }    
